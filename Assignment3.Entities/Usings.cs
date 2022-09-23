1b73f6b6a2e288b4377cfd3943cc1660a4d72bdb
Anne-Marie <annemarierommerdahl@gmail.com>
2022-09-23 15:46:55 +0200

    set up context

---
+++ Assignment3.Entities/Usings.cs
@@ -0,0 +1,5 @@
+global using System.ComponentModel.DataAnnotations;
+global using Assignment3.Core;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata;
+
