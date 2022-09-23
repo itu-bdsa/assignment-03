0193ee8aa769fe634dbccfc0a99913b6032ac96f
Lucasfth <luha@itu.dk>
2022-09-23 14:36:25 +0200

    setup KanbanContext

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -1,5 +1,55 @@
-namespace Assignment3.Entities;
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore;
 
-public class KanbanContext
+namespace Assignment3.Entities
 {
+    public partial class KanbanContext : DbContext
+    {
+        public KanbanContext(DbContextOptions<KanbanContext> options)
+            : base(options)
+        {
+        }
+        
+        public virtual DbSet<Tag> Tags { get; set; } = null!;
+        public virtual DbSet<Task> Tasks { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.Property(e => e.Id);
+                entity.Property(e => e.Title).HasMaxLength(100);
+                entity.Property(e => e.AssignedTo);
+                entity.Property(e => e.Description);
+                entity.Property(e => e.State);
+                entity.Property(e => e.Tags);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id);
+                entity.Property(e => e.Name).HasMaxLength(100);
+                entity.Property(e => e.Email).HasMaxLength(100);
+                entity.Property(e => e.Tasks);
+            });
+
+            modelBuilder.Entity<Tag>(entity =>
+            {
+                entity.Property(e => e.Id);
+                entity.Property(e => e.Name).HasMaxLength(50);
+                entity.Property(e => e.Tasks);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
 }
--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -2,4 +2,19 @@ namespace Assignment3.Entities;
 
 public class Task
 {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public User AssignedTo { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public StateEnum State { get; set; }
+    public ICollection<Tag> Tags { get; set; }
+}
+
+public enum StateEnum
+{
+    New,
+    Active,
+    Resolved,
+    Closed,
+    Removed,
 }
