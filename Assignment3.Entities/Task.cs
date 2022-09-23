18debb125dd691ff96e58b540c7459a2b2b63839
Amalie (amdh) <amdh@itu.dk>
2022-09-23 14:03:25 +0200

    Tasks started

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -1,5 +1,19 @@
 namespace Assignment3.Entities;
 
-public class Task
+public partial class Task
 {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public User AssignedTo {get; set;} = null!;
+    public string Description { get; set; } = null!;
+    public State state { get; set; }
+    public List<Tag> Tags {get; set;}
+    
+}
+public enum State{
+    New,
+    Active,
+    Resolved,
+    Closed,
+    Removed
 }
