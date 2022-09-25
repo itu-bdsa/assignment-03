26f6dc68e861b066fe79dae9fa52cd9b6ca6a5b1
ChrBank <chbl@itu.dk>
2022-09-25 13:48:58 +0200

    implement test and TagRepository

--- Assignment3.Entities.Tests/TagRepositoryTests.cs
+++ Assignment3.Entities.Tests/TagRepositoryTests.cs
@@ -7,41 +7,11 @@ namespace Assignment3.Entities.Tests;
 public class TagRepositoryTests
 {
     [Fact]
-    public void CreateAlreadyExistingTagShouldReturnConflictAndId()
+    public void Create_Already_Existing_Tag_Should_Return_Conflict_And_Id()
     {
         // Arrange
-        Tag tag = new Tag();
-        tag.Name = "MyName";
-        
-        TagCreateDTO record = new TagCreateDTO("MyName");
-        
-        // Act
-        var output = tag.Create(record);
-        
-        // Assert
-        Assert.Equal((Response.Conflict, tag.Id), output);
-    }
-
-    [Fact]
-    public void CreateDoesNotExistTagShouldReturnCreatedAndId()
-    {
-        // Arrange
-        Tag tag = new Tag();
-        tag.Name = "Bo";
-        TagCreateDTO record = new TagCreateDTO("MyName");
-        
-        // Act
-        var output = tag.Create(record);
-        
-        // Assert
-        Assert.Equal((Response.Created, tag.Id), output);
-    }
-
-    [Fact]
-    public void ReadAllShouldReturnReadCollectionOfAllTasksTags()
-    {
-        // Arrange
-        Tag dad = new Tag();
+        TagRepository tagRepo= new TagRepository();
+       
         Tag son1 = new Tag();
         Tag son2 = new Tag();
         Tag son3 = new Tag();
@@ -55,45 +25,76 @@ public class TagRepositoryTests
         son4.Name = "SonnyB";
         son4.Id = 4;
 
-        Task task1 = new Task();
-        Task task2 = new Task();
-
-        var list1 = new Collection<Tag>();
-        var list2 = new Collection<Tag>();
-        
-        list1.Add(son1);
-        list2.Add(son2);
-        list2.Add(son3);
-        list2.Add(son4);
-
-        task1.Tags = list1;
-        task2.Tags = list2;
-
-        var tasks = new Collection<Task>();
-        tasks.Add(task1);
-        tasks.Add(task2);
-
-        dad.Tasks = tasks;
-
-        var list = new Collection<TagDTO>();
-        list.Add(new TagDTO(son1.Id, son1.Name));
-        list.Add(new TagDTO(son2.Id, son2.Name));
-        list.Add(new TagDTO(son3.Id, son3.Name));
-        list.Add(new TagDTO(son4.Id, son4.Name));
-        var expected = new ReadOnlyCollection<TagDTO>(list);
+        tagRepo.tags.Add(son1);
+        tagRepo.tags.Add(son2);
+        tagRepo.tags.Add(son3);
+        tagRepo.tags.Add(son4);
+           
         
         // Act
-        var output = dad.ReadAll();
+        var output = tagRepo.Create(new TagCreateDTO("SonnyB"));
+        
+        // Assert
+        Assert.Equal((Response.Conflict, 4), output);
+    }
+
+    [Fact]
+    public void Create_Does_Not_Exist_Tag_Should_Return_Created_And_Id()
+    {
+        // Arrange
+        TagRepository tagRepo= new TagRepository();
+        
+        // Act
+        var output = tagRepo.Create(new TagCreateDTO("MyName"));
+        
+        // Assert
+        Assert.Equal((Response.Created, 0), output);
+    }
+
+    [Fact]
+    public void ReadAll_Should_Return_ReadCollection_Of_All_Tasks_Tags()
+    {
+        // Arrange
+        TagRepository tagRepo = new TagRepository();
+
+        Tag son1 = new Tag();
+        Tag son2 = new Tag();
+        Tag son3 = new Tag();
+        Tag son4 = new Tag();
+        son1.Name = "Son";
+        son1.Id = 1;
+        son2.Name = "Sonn";
+        son2.Id = 2;
+        son3.Name = "Sonny";
+        son3.Id = 3;
+        son4.Name = "SonnyB";
+        son4.Id = 4;
+
+        tagRepo.tags.Add(son1);
+        tagRepo.tags.Add(son2);
+        tagRepo.tags.Add(son3);
+        tagRepo.tags.Add(son4);
+        
+        var temp = new Collection<TagDTO>();
+        temp.Add(new TagDTO(son1.Id, son1.Name));
+        temp.Add(new TagDTO(son2.Id, son2.Name));
+        temp.Add(new TagDTO(son3.Id, son3.Name));
+        temp.Add(new TagDTO(son4.Id, son4.Name));
+
+        var expected = new ReadOnlyCollection<TagDTO>(temp);
+        // Act
+        var output = tagRepo.ReadAll();
         
         // Assert
         Assert.Equal(expected, output);
     }
 
     [Fact]
-    public void ReadShouldOnlyReturnSon1Given1()
+    public void Read_Should_Only_Return_Son1_Given1()
     {
-        // Arrange
-        Tag dad = new Tag();
+         // Arrange
+        TagRepository tagRepo = new TagRepository();
+
         Tag son1 = new Tag();
         Tag son2 = new Tag();
         Tag son3 = new Tag();
@@ -107,38 +108,90 @@ public class TagRepositoryTests
         son4.Name = "SonnyB";
         son4.Id = 4;
 
-        Task task1 = new Task();
-        Task task2 = new Task();
-
-        var list1 = new Collection<Tag>();
-        var list2 = new Collection<Tag>();
-        
-        list1.Add(son1);
-        list2.Add(son2);
-        list2.Add(son3);
-        list2.Add(son4);
-
-        task1.Tags = list1;
-        task2.Tags = list2;
-
-        var tasks = new Collection<Task>();
-        tasks.Add(task1);
-        tasks.Add(task2);
-
-        dad.Tasks = tasks;
+        tagRepo.tags.Add(son1);
+        tagRepo.tags.Add(son2);
+        tagRepo.tags.Add(son3);
+        tagRepo.tags.Add(son4);
         
         // Act
-        var actual = dad.Read(1);
+        var output = tagRepo.Read(1);
         
         // Assert
-        Assert.Equal(new TagDTO(1, "Son"), actual);
+        Assert.Equal(new TagDTO(1, "Son"), output);
     }
-    
+
+     [Fact]
+    public void Update_Should_Respond_Updated_If_Name_Is_New()
+    {
+        // Arrange
+        TagRepository tagRepo = new TagRepository();
+
+
+        Tag son1 = new Tag();
+        Tag son2 = new Tag();
+        Tag son3 = new Tag();
+        Tag son4 = new Tag();
+        son1.Name = "Son";
+        son1.Id = 1;
+        son2.Name = "Sonn";
+        son2.Id = 2;
+        son3.Name = "Sonny";
+        son3.Id = 3;
+        son4.Name = "SonnyB";
+        son4.Id = 4;
+
+        tagRepo.tags.Add(son1);
+        tagRepo.tags.Add(son2);
+        tagRepo.tags.Add(son3);
+        tagRepo.tags.Add(son4);
+
+        
+       
+
+        // Act
+        var actual = tagRepo.Update(new TagUpdateDTO(1, "SonTheMan"));
+        // Assert
+        Assert.Equal(Response.Updated, actual);
+    }
+
+       [Fact]
+    public void Update_Should_Respond_BadRequest_If_None_Is_Updated()
+    {
+        // Arrange
+        TagRepository tagRepo = new TagRepository();
+
+
+        Tag son1 = new Tag();
+        Tag son2 = new Tag();
+        Tag son3 = new Tag();
+        Tag son4 = new Tag();
+        son1.Name = "Son";
+        son1.Id = 1;
+        son2.Name = "Sonn";
+        son2.Id = 2;
+        son3.Name = "Sonny";
+        son3.Id = 3;
+        son4.Name = "SonnyB";
+        son4.Id = 4;
+
+        tagRepo.tags.Add(son1);
+        tagRepo.tags.Add(son2);
+        tagRepo.tags.Add(son3);
+        tagRepo.tags.Add(son4);
+
+        // Act
+        var actual = tagRepo.Update(new TagUpdateDTO(1, "Son"));
+        // Assert
+        Assert.Equal(Response.NotFound, actual);
+    }
+
     [Fact]
-    public void ReadShouldReturnNegativeOneGiven5()
+    public void Delete_Should_Respond_Deleted()
     {
         // Arrange
-        Tag dad = new Tag();
+        TagRepository tagRepo = new TagRepository();
+
+
         Tag son1 = new Tag();
         Tag son2 = new Tag();
         Tag son3 = new Tag();
@@ -152,32 +205,16 @@ public class TagRepositoryTests
         son4.Name = "SonnyB";
         son4.Id = 4;
 
-        Task task1 = new Task();
-        Task task2 = new Task();
+        tagRepo.tags.Add(son1);
+        tagRepo.tags.Add(son2);
+        tagRepo.tags.Add(son3);
+        tagRepo.tags.Add(son4);
 
-        var list1 = new Collection<Tag>();
-        var list2 = new Collection<Tag>();
-        
-        list1.Add(son1);
-        list2.Add(son2);
-        list2.Add(son3);
-        list2.Add(son4);
-
-        task1.Tags = list1;
-        task2.Tags = list2;
-
-        var tasks = new Collection<Task>();
-        tasks.Add(task1);
-        tasks.Add(task2);
-
-        dad.Tasks = tasks;
-        
         // Act
-        var actual = dad.Read(5);
-        
-        // Assert
-        Assert.Equal(new TagDTO(-1, ""), actual);
-    }
+        var actual = tagRepo.Delete(1);
     
+        // Assert
+        Assert.Equal(Response.Deleted, actual);
+    }
     
 }
--- Assignment3.Entities/TagRepository.cs
+++ Assignment3.Entities/TagRepository.cs
@@ -1,5 +1,74 @@
 namespace Assignment3.Entities;
+using Assignment3.Core;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Tracing;
 
-public class TagRepository
+public class TagRepository : ITagRepository
 {
+
+    public Collection<Tag> tags = new Collection<Tag>();
+
+
+    public (Response Response, int TagId) Create(TagCreateDTO tag)
+    {
+        foreach (var t in tags)
+        {
+            if (t.Name.Equals(tag.Name)){
+                return (Response.Conflict, t.Id);
+            }
+        }
+        Tag tg = new Tag();
+        tg.Name = tag.Name;
+        return (Response.Created, tg.Id);                         // Should maybe be switched to another Id
+    }
+
+    public IReadOnlyCollection<TagDTO> ReadAll()
+    {
+        var temp = new Collection<TagDTO>();
+        
+        foreach (var tag in tags)
+        {
+           temp.Add(new TagDTO(tag.Id, tag.Name));
+        }
+
+        return new ReadOnlyCollection<TagDTO>(temp);
+    }
+
+    public TagDTO Read(int tagId)
+    {
+        foreach (var tag in ReadAll())
+        {
+            if (tag.Id == tagId)
+            {
+                return tag;
+            }
+        }
+
+        return new TagDTO(-1, "");
+    }
+
+    public Response Update(TagUpdateDTO tag)
+    {   
+        foreach (var t in tags)
+        {   
+            if (t.Id == tag.Id && !t.Name.Equals(tag.Name)){
+                t.Name = tag.Name;
+                return Response.Updated;
+            }
+        }  
+        return Response.NotFound;
+    }
+
+    public Response Delete(int tagId, bool force = false)
+    {
+        foreach (var t in tags)
+        {
+            if(t.Id == tagId){
+                tags.Remove(t);
+                return Response.Deleted;
+            }
+        }
+        return Response.NotFound;
+
+    }
 }
