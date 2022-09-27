933486b74c8f985569f68d598218a8f221a0ebbe
Amalie (amdh) <amdh@itu.dk>
2022-09-27 13:45:32 +0200

    Update repositories

---
+++ Assignment3.Core/IWorkItemRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment3.Core;
+
+public interface IWorkItemRepository
+{
+    (Response Response, int WorkItemId) Create(WorkItemCreateDTO workItem);
+    IReadOnlyCollection<WorkItemDTO> ReadAll();
+    IReadOnlyCollection<WorkItemDTO> ReadAllRemoved();
+    IReadOnlyCollection<WorkItemDTO> ReadAllByTag(string tag);
+    IReadOnlyCollection<WorkItemDTO> ReadAllByUser(int userId);
+    IReadOnlyCollection<WorkItemDTO> ReadAllByState(State state);
+    WorkItemDetailsDTO Read(int workItemId);
+    Response Update(WorkItemUpdateDTO WorkItem);
+    Response Delete(int workItemId);
+}
