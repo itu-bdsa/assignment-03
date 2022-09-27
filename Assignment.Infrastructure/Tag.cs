2d5240fb7c51092adb12b79e6dafcf9f490ffc58
Maxime Birkkjær Havez <maxih@hotmail.dk>
2022-09-27 14:00:47 +0200

    Removed unnecesary files and folders

--- Assignment.Infrastructure/Tag.cs
+++ Assignment.Infrastructure/Tag.cs
@@ -2,4 +2,10 @@ namespace Assignment.Infrastructure;
 
 public class Tag
 {
+    public int Id{get;}
+
+    [StringLength(100)]
+    public string? Name{get;set;}
+
+    
 }
--- Assignment.Infrastructure/User.cs
+++ Assignment.Infrastructure/User.cs
@@ -1,5 +1,13 @@
 namespace Assignment.Infrastructure;
 
-public class User
-{
+public class User{
+    public int Id{get;}
+
+    [StringLength(100)]
+    public string? Name{get;set;}
+    [StringLength(100)]
+    public string? Email{get;set;}
+
+    
+
 }
