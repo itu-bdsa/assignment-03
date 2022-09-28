ff9fc79c1e4caec56bd017e635cf9c2a0691c362
Silas Bue Handgaard <silasbueh@hotmail.com>
2022-09-28 10:29:59 +0200

    add dbcontext

--- Assignment.Infrastructure/KanbanContext.cs
+++ Assignment.Infrastructure/KanbanContext.cs
@@ -1,5 +1,27 @@
-namespace Assignment.Infrastructure;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
-public class KanbanContext
+namespace Assignment.Infrastructure
 {
+  public partial class kanbanContext : DbContext
+  {
+    public kanbanContext()
+    {
+    }
+
+    public kanbanContext(DbContextOptions<kanbanContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  }
 }
--- Assignment.Infrastructure/WorkItem.cs
+++ Assignment.Infrastructure/WorkItem.cs
@@ -2,20 +2,21 @@ namespace Assignment.Infrastructure;
 
 public class WorkItem
 {
-    public enum State{
-        New, Active, Resolved, Closed, Removed
-    }
 
 
-    public int Id { get; set; }
-    [StringLength(100)]
-    public string Title {get;set;}
+  public int Id { get; set; }
+  [StringLength(100)]
+  public string? Title { get; set; }
 
-    public int AssignedTo{get; set;}
-    public string? Description{get; set;}
+  public int AssignedTo { get; set; }
+  public string? Description { get; set; }
 
-    public State State{get;set;}
+  public State State { get; set; }
 
-    // public 
+  // public
 
+}
+public enum State
+{
+  New, Active, Resolved, Closed, Removed
 }
